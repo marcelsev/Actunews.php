6fee8df562e79a1eb55a4ff384443b1d3a5fa202
marcelsev <marcelosolanosev@gmail.com>
2023-09-21 10:57:20 +0200

    article.php

--- article.php
+++ article.php
@@ -1,38 +1,99 @@
 <?php
-    # Inclusion du header
-    require_once './components/header.php';
-     /*
-     * La superglobale GET me permet de récupérer les informations
-     * passées dans mon URL. Ex. categorie.php?slug=politique
-     */
-    //var_dump($_GET);
+
+# Inclusion du header
+
+require_once './components/header.php';
+
 ?>
 
+ 
+
 <!-- Contenu de notre page -->
+
 <!-- .p-3.mx-auto.text-center>h1.display-4{Actunews} -->
+
 <main>
 
-    
-    <!-- Contenu de la page -->
-    <!-- .py-5.bg-light>.container>.row>.col-md-4*6>.card.shadow-sm -->
-    <div class="py-5 bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <!-- Titre de la page -->
-                    <h1 class="display-4"></h1>
-                    <hr class="border border-dark mb-5">
+ 
 
-                    <h4 class="text-muted d-flex justify-content-end align-items-center"><small></small></h4>
+    <!-- Contenu de la page -->
+
+    <!-- .py-5.bg-light>.container>.row>.col-md-4*6>.card.shadow-sm -->
+
+    <div class="py-5 bg-light">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col">
+
+ 
+
+                    <!-- Titre de l'article -->
+
+                    <h1 class="display-4">Popularité, prises de position, affaires de famille... Quel bilan pour Charles III un an
+
+                        après son arrivée sur le trône ?</h1>
+
+ 
+
+                    <hr class="border border-dark" >
+
+ 
+
+                    <h4 class="text-muted d-flex justify-content-between align-items-center">
+
+                        <p class="m-0"><small style="font-size: 1rem; margin-right: 1rem;">Publié le</small> 21/09/2023 06:10</p>
+
+                        <p class="m-0"><small style="font-size: 1rem; margin-right: 1rem;">Rédigé par </small> Hugo LIEGEARD</p>
+
+                    </h4>
+
+ 
+
+                    <hr class="border border-dark mb-5" >
+
+ 
+
+                    <!-- Illustration de l'article -->
+
+                    <img class="img-fluid mb-5" src="https://www.francetvinfo.fr/pictures/mIDWEOEviZA7Iqw5kfG_X0Mnsnk/0x56:4000x2305/2656x1494/filters:format(avif):quality(50)/2023/09/05/000-33ej93q-64f72d09277ea609775318.jpg"
+
+                         alt="[TITRE]">
+
+ 
+
+                    <!-- Contenu de l'article' -->
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum, dolore earum eligendi error fugiat labore maxime minus nobis officiis optio perferendis quaerat quas quo quos sint unde velit voluptatum.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum, dolore earum eligendi error fugiat labore maxime minus nobis officiis optio perferendis quaerat quas quo quos sint unde velit voluptatum.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum, dolore earum eligendi error fugiat labore maxime minus nobis officiis optio perferendis quaerat quas quo quos sint unde velit voluptatum.</p>
+
+ 
+
                 </div>
+
             </div>
+
         </div>
+
     </div>
 
+ 
+
 </main>
+
 <!-- Fin -- Contenu de notre page -->
 
+ 
+
 <?php
+
 # Inclusion du header
+
 require_once './components/footer.php';
+
 ?>
